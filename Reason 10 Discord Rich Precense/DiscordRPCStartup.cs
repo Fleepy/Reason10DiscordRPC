822a6a548202e4bbde9751668a96a4c37ddb5a08
Flleeppyy <flleeppyy@gmail.com>
2020-01-11 00:29:57 -0800

    Yeet

---
+++ Reason 10 Discord Rich Precense/DiscordRPCStartup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DiscordRPC;
+using DiscordRPC.Logging;
+using DiscordRPC.IO;
+using DiscordRPC.Events;
+using DiscordRPC.Exceptions;
+using DiscordRPC.Message;
+using DiscordRPC.Helper;
+
+namespace Reason10DRP
+{
+    internal class DiscordRPCStartup
+    {
+		private static DiscordRpcClient client;
+
+		public static void DRPCC()
+        {
+			client = new DiscordRpcClient("524248942465253416");
+
+			//Set the logger
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Info };
+
+			//Subscribe to events
+			client.OnReady += (sender, e) =>
+			{
+				Console.WriteLine("Received Ready from user {0}", e.User.Username);
+			};
+
+			client.OnPresenceUpdate += (sender, e) =>
+			{
+				Console.WriteLine("Received Update! {0}", e.Presence);
+			};
+
+			//Connect to the RPC
+			client.Initialize();
+
+			//Set the rich presence
+			//Call this as many times as you want and anywhere in your code.
+			client.SetPresence(new RichPresence()
+			{
+				Details = "Example Project",
+				State = "csharp example",
+				Assets = new Assets()
+				{
+					LargeImageKey = "image_large",
+					LargeImageText = "Reason Studios",
+					SmallImageKey = "image_small"
+				}
+			});
+		}
+		public static void ExitDiscordRPC()
+		{
+			client.Dispose();
+		}
+	}
+}
---
+++ Reason 10 Discord Rich Precense/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reason10DRP
+{
+
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            DiscordRPCStartup.DRPCC();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+            void OnProcessExit(object sender, EventArgs e)
+            {
+                DiscordRPCStartup.ExitDiscordRPC();
+            }
+        }
+        
+    }
+
+}
